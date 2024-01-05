4b27ca5bdc6e436cff55b29a65dc1d6b5097c340
Kian738 <56565697+Kian738@users.noreply.github.com>
2024-01-05 20:03:11 +0100

    Simplify StringUtils

--- Quantum/Source/Utils/StringUtils.cpp
+++ Quantum/Source/Utils/StringUtils.cpp
@@ -2,33 +2,27 @@
 
 namespace Quantum
 {
-	String StringUtils::BeforeLast(const char* string, const char* delimiter)
+	String StringUtils::BeforeLast(StringView string, StringView delimiter)
 	{
-		return BeforeLast<String>(string, delimiter);
+		auto result = String(string);
+		UInt64 index = string.find_last_of(delimiter);
+		if (index != StringView::npos)
+			result = string.substr(0, index);
+		return result;
 	}
 
-	String StringUtils::BeforeLast(String string, String delimiter)
-	{
-		return BeforeLast<String>(string, delimiter);
-	}
-
-	StringView StringUtils::BeforeLast(StringView string, StringView delimiter)
-	{
-		return BeforeLast<StringView>(string, delimiter);
-	}
-
-	List<String> StringUtils::Split(const char* string, const char* delimiter)
-	{
-		return Split<String>(string, delimiter);
-	}
-
-	List<String> StringUtils::Split(String string, String delimiter)
-	{
-		return Split<String>(string, delimiter);
-	}
-
-	List<StringView> StringUtils::Split(StringView string, StringView delimiter)
+	List<String> StringUtils::Split(StringView string, StringView delimiter)
     {
-		return Split<StringView>(string, delimiter);
+		List<String> result;
+		UInt64 start = 0;
+		UInt64 end = string.find(delimiter);
+		while (end != StringView::npos)
+		{
+			result.push_back(String(string.substr(start, end - start)));
+			start = end + delimiter.size();
+			end = string.find(delimiter, start);
+		}
+		result.push_back(String(string.substr(start, end)));
+		return result;
     }
 }
--- Quantum/Source/Utils/StringUtils.h
+++ Quantum/Source/Utils/StringUtils.h
@@ -7,37 +7,7 @@ namespace Quantum
 	class StringUtils
 	{
 	public:
-		static String BeforeLast(const char* string, const char* delimiter);
-		static String BeforeLast(String string, String delimiter);
-		static StringView BeforeLast(StringView string, StringView delimiter);
-
-		static List<String> Split(const char* string, const char* delimiter);
-		static List<String> Split(String string, String delimiter);
-		static List<StringView> Split(StringView string, StringView delimiter);
-	private:
-		template <typename T>
-		static T BeforeLast(T string, T delimiter)
-		{
-			UInt64 index = string.find_last_of(delimiter);
-			if (index != T::npos)
-				return string.substr(0, index);
-			return string;
-		};
-
-		template <typename T>
-		static List<T> Split(T string, T delimiter)
-		{
-			List<T> result;
-			UInt64 start = 0;
-			UInt64 end = string.find(delimiter);
-			while (end != T::npos)
-			{
-				result.push_back(string.substr(start, end - start));
-				start = end + delimiter.size();
-				end = string.find(delimiter, start);
-			}
-			result.push_back(string.substr(start, end));
-			return result;
-		};
+		static String BeforeLast(StringView string, StringView delimiter);
+		static List<String> Split(StringView string, StringView delimiter);
 	};
 }
